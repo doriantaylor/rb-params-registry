1edf70c81f70d018391c680378da410b6106ab0d
Dorian Taylor <code@doriantaylor.com>
2023-09-19 22:01:59 -0400

    initial commit of stub directory

---
+++ lib/params/registry.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "registry/version"
+
+module Params
+  module Registry
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
